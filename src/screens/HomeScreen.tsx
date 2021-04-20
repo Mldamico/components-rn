008f4c3053362272d6d4578316cbbdebb5aa3096
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-20 13:52:18 -0300

    Flat list header and separator

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
+import {FlatList, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {appTheme} from '../theme/appTheme';
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  components: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    components: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    components: 'Animation102Screen',
+  },
+];
+
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.name} - <Icon name={menuItem.icon} size={30} />
+        </Text>
+      </View>
+    );
+  };
+
+  const renderListHeader = () => {
+    const {top} = useSafeAreaInsets();
+    return (
+      <View style={{marginTop: 20 + top, marginBottom: 20}}>
+        <Text style={appTheme.title}>Opciones de menu</Text>
+      </View>
+    );
+  };
+
+  const itemSeparator = () => {
+    return <View style={appTheme.listSeparator}></View>;
+  };
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Icon name="star-outline" size={20} />
+    <View style={{flex: 1, ...appTheme.globalMargin}}>
+      <FlatList
+        ListHeaderComponent={renderListHeader}
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+        ItemSeparatorComponent={itemSeparator}
+      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const appTheme = StyleSheet.create({
+  globalMargin: {
+    marginHorizontal: 20,
+  },
+  title: {fontSize: 35, fontWeight: 'bold'},
+  listSeparator: {
+    borderBottomWidth: 1,
+    marginVertical: 5,
+    opacity: 0.4,
+  },
+});
